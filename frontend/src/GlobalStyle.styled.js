9589e7b734c17e761353a46157396291e312328a
ailen munoz <ailenmunozz@gmail.com>
2019-11-15 11:57:02 -0300

    Fix google fonts en produccion

--- frontend/src/GlobalStyle.styled.js
+++ frontend/src/GlobalStyle.styled.js
@@ -1,16 +1,16 @@
 import { createGlobalStyle } from 'styled-components';
 import { colors, font } from './styles/theme';
 
-
 export default createGlobalStyle`
+  @import url(https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700|Roboto:300,400,700&display=swap);
+
 html {
-  @import url('https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700|Roboto:300,400,700&display=swap');
-  font-family: ${font.family};
   font-size: ${font.module};
   scroll-behavior: smooth;
   scrollbar-width: none;
 }
 body {
+  font-family: ${font.family};
   background: ${colors.background};
   margin: 0;
   padding: 0;
