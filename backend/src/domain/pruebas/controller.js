d1ddd98415814e61647450ba9a666a96243fb594
fgmf94 <f_gmf94@hotmail.com>
2019-11-20 17:10:09 -0300

    se informan los errores como respuesta

--- backend/src/domain/pruebas/controller.js
+++ backend/src/domain/pruebas/controller.js
@@ -4,19 +4,19 @@ const PruebasController = ({buscarTemasVotacionRoots, buscarTemasGuardados, guar
   temas: (req, res) => {
     buscarTemasVotacionRoots().then(temas => {
         res.status(200).send(temas);
-    });
+    }).catch(error => res.status(500).send(error));
   },
 
   guardarTemas: (req, res) => {
     guardarTemasVotacionRoots().then(temas => {
         res.status(200).send();
-    });
+    }).catch(error => res.status(500).send(error));
   },
 
   devolverTemasGuardados: (req, res) => {
     buscarTemasGuardados().then(temas =>
       res.status(200).send(temas)
-      );
+    ).catch(error => res.status(500).send(error));
   }
 
 });
