e50d19fca5ffec09de79101836efc229b5b29b2c
Carolina <carolina.destuet@10pines.com>
2019-11-28 13:21:43 -0300

    imagenes correspondientes a duracion y obligatoriedad

--- frontend/src/temario/InfoTema.js
+++ frontend/src/temario/InfoTema.js
@@ -7,8 +7,8 @@ class InfoTema extends React.Component {
     return (
       <InfoTemaContainer>
         <InfoItem src="./usuarie.svg" altText="Usuarie" descripcion={this.props.autor} position="20px 20px" size="80%"/>
-        <InfoItem src="./pino.png" altText="Pino" descripcion={`${this.props.duracion} min.`} position="8px" size="90%"/>
-        <InfoItem src="./tema-obligatorio.svg" altText="Obligatorio" descripcion={obligatoriedad[this.props.obligatoriedad]} position="35px" size="45%"/>
+        <InfoItem src={diccDuracionTema[this.props.duracion]} altText="Pino" descripcion={`${this.props.duracion} min.`} position="8px" size="90%"/>
+        <InfoItem src={diccObligatoriedad[this.props.obligatoriedad]} descripcion={obligatoriedad[this.props.obligatoriedad]} position="35px" size="45%"/>
       </InfoTemaContainer>
     );
   }
@@ -17,6 +17,17 @@ class InfoTema extends React.Component {
 const obligatoriedad = {
   OBLIGATORIO: 'Obligatorio',
   NO_OBLIGATORIO: 'No obligatorio',
+};
+
+const diccDuracionTema = {
+  30: './tema-corto.svg',
+  60: './tema-mediano.svg',
+  90: './tema-largo.svg',
+};
+
+const diccObligatoriedad = {
+  OBLIGATORIO: './tema-obligatorio.svg',
+  NO_OBLIGATORIO: './tema-no-obligatorio.svg',
 };
 
 export default InfoTema;
