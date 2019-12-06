81a24207b5b7263aee09caa9d65006c37d5e1e74
fgmf94 <f_gmf94@hotmail.com>
2019-12-06 13:59:25 -0300

    mejoras visuales a los botones

--- frontend/src/components/Button.styled.js
+++ frontend/src/components/Button.styled.js
@@ -3,11 +3,24 @@ import { colors, font } from '../styles/theme';
 
 export const Button = styled.button`
   font-size: ${font.sizeP};
-  background: ${colors.primary};
+  background: ${(props) => (!props.disabled ? colors.primary : colors.black30)};
   border-radius: 0.2em;
   border: none;
   padding: 0.5em 3em;
   cursor: pointer;
-  color: white;
-  font-family: ${font.p}
+  color: ${(props) => (!props.disabled ? colors.white : colors.black50)};
+  font-family: ${font.p};
+  &:hover {
+    background: ${(props) => (!props.disabled ? colors.viridian : colors.black30)};
+  }
+`;
+
+export const SecondaryButton = styled(Button)`
+  border: 0.07em solid;
+  border-color: ${(props) => (!props.disabled ? colors.downy : colors.black30)};
+  background: ${colors.white};
+  color: ${(props) => (!props.disabled ? colors.downy : colors.black40)};
+  &:hover {
+    background: ${(props) => (!props.disabled ? colors.black10 : colors.white)};
+  }
 `;
