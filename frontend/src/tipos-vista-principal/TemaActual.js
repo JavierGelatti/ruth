deb48167bc5b3273e4d9009617e440f533f6f144
Ornella M <ornellamcfly@gmail.com>
2019-12-10 12:44:15 -0300

    Otra mejora para que no aparezca el toast de que no se pudo actualizar el tema al cerrar la reunión, si es que el tema nunca se inició

--- frontend/src/tipos-vista-principal/TemaActual.js
+++ frontend/src/tipos-vista-principal/TemaActual.js
@@ -23,7 +23,9 @@ class TemaActual extends React.Component {
   static canHandleView = (view) => view === 'Tema Actual'
 
   handleCerrarReunion = () => {
-    this.handleTerminarTema();
+    if (this.props.temaActivo) {
+      this.handleTerminarTema();
+    }
     backend.cerrarReunion()
       .then(() => this.setState({ redirect: true }));
   }
