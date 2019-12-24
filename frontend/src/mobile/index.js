1403fe21b33f6431b694a1d370cb4bb900671ca7
Orne <ornellamcfly@gmail.com>
2019-12-24 10:24:27 -0300

    Creación de rutas /mobile y /oradores

---
+++ frontend/src/mobile/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Mobile extends React.Component {
+    render(){
+        return(
+            <h1>Mobile</h1>
+        )
+    }
+}
+
+
+export default Mobile;
---
+++ frontend/src/oradores/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Oradores extends React.Component {
+    render(){
+        return(
+            <h1>Oradores</h1>
+        )
+    }
+}
+
+
+export default Oradores;
