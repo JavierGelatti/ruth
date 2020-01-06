426e5c532f035623ff49e314ed5aaa498798ad9c
Orne <ornellamcfly@gmail.com>
2020-01-06 12:59:34 -0300

    Se pasaron a slyled components algunas constantes que estaban en los archivos de los componentes de react

--- frontend/src/cola-de-participantes/ParticipantsQueue.js
+++ frontend/src/cola-de-participantes/ParticipantsQueue.js
@@ -1,45 +1,8 @@
 import React from 'react';
-import { QueueContainer } from './ParticipantsQueue.styled';
+import {
+  QueueContainer, QueuedLeftCardsStyle, QueuedCardsLeftContainerStyle, QueuedRightCardsStyle, QueuedCardsRightContainerStyle,
+} from './ParticipantsQueue.styled';
 import ParticipantsCard from './ParticipantsCard';
-
-const queuedLeftCardsStyle = {
-  display: 'flex',
-  flexDirection: 'column',
-  width: '40rem',
-  justifyContent: 'center',
-  paddingTop: '1rem',
-  alignItems: 'center',
-};
-
-const queuedRightCardsStyle = {
-  display: 'flex',
-  flexDirection: 'column',
-  width: '40rem',
-  justifyContent: 'center',
-  paddingTop: '1rem',
-  alignItems: 'center',
-  opacity: '0.5',
-};
-
-const queuedCardsLeftContainerStyle = {
-  display: 'flex',
-  flexDirection: 'row',
-  alignItems: 'center',
-  borderRight: '1px solid silver',
-  paddingRight: '1rem',
-  justifyContent: 'flex-end',
-  width: '100%',
-};
-
-const queuedCardsRightContainerStyle = {
-  display: 'flex',
-  flexDirection: 'row',
-  alignItems: 'center',
-  borderLeft: '1px solid silver',
-  paddingLeft: '1rem',
-  width: '100%',
-
-};
 
 class ParticipantsQueue extends React.Component {
     getQueuedParticipants = () => this.props.participants.filter((participant) => participant.secondsElapsed === 0);
@@ -51,19 +14,19 @@ class ParticipantsQueue extends React.Component {
     render() {
       return (
           <QueueContainer>
-            <div style={queuedLeftCardsStyle}>
-              <div style={queuedCardsLeftContainerStyle}>
+            <QueuedLeftCardsStyle>
+              <QueuedCardsLeftContainerStyle>
                 { this.getQueuedParticipants().map((participant, index) => <ParticipantsCard participant={participant} key={index}/>)}
-              </div>
-            </div>
+              </QueuedCardsLeftContainerStyle>
+            </QueuedLeftCardsStyle>
             <div>
               <ParticipantsCard participant={this.getTalkingParticipant()}/>
             </div>
-            <div style={queuedRightCardsStyle}>
-              <div style={queuedCardsRightContainerStyle}>
+            <QueuedRightCardsStyle>
+              <QueuedCardsRightContainerStyle>
                 { this.getParticipantsThatAlreadyTalked().map((participant, index) => <ParticipantsCard participant={participant} key={index}/>)}
-              </div>
-            </div>
+              </QueuedCardsRightContainerStyle>
+            </QueuedRightCardsStyle>
           </QueueContainer>
       );
     }
--- frontend/src/cola-de-participantes/ParticipantsQueue.styled.js
+++ frontend/src/cola-de-participantes/ParticipantsQueue.styled.js
@@ -1,9 +1,47 @@
 import styled from 'styled-components';
-import { font, translucid } from '../styles/theme';
 
 export const QueueContainer = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: center;
+`;
+
+export const QueuedLeftCardsStyle = styled.div`
+  display: 'flex',
+  flexDirection: 'column',
+  width: '40rem',
+  justifyContent: 'center',
+  paddingTop: '1rem',
+  alignItems: 'center',
+`;
+
+export const QueuedRightCardsStyle = styled.div`
+  display: 'flex',
+  flexDirection: 'column',
+  width: '40rem',
+  justifyContent: 'center',
+  paddingTop: '1rem',
+  alignItems: 'center',
+  opacity: '0.5',
+`;
+
+export const QueuedCardsLeftContainerStyle = styled.div`
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  borderRight: '1px solid silver',
+  paddingRight: '1rem',
+  justifyContent: 'flex-end',
+  width: '100%',
+`;
+
+export const QueuedCardsRightContainerStyle = styled.div`
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  borderLeft: '1px solid silver',
+  paddingLeft: '1rem',
+  width: '100%',
+
 `;
