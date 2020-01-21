916910ce25de476d32e153188cf1c6039f41ae7b
Carolina <carolinadestuet@gmail.com>
2020-01-21 11:49:17 -0300

    fix detalles de action items, se detalla mas info

--- frontend/src/temario/descripcion-tipo-tema/DescripcionActionItems.js
+++ frontend/src/temario/descripcion-tipo-tema/DescripcionActionItems.js
@@ -6,24 +6,27 @@ import {
 class DescripcionActionItems extends React.Component {
   render() {
     return (
-            <DescripcionTemaContainer>
-                <Titulo>{this.props.tema.titulo}</Titulo>
-                <Descripcion>
-                    <ListaTemasARepasar>
-                        {this.props.tema.temasParaRepasar.map((temaReunionAnterior, index) => 
-                        <li key={`tema-a-repasar-${index}`}>
-                            <b> {temaReunionAnterior.tema.titulo} </b>
-                            <p> {temaReunionAnterior.tema.descripcion} </p>
-                            <ul>
-                              {temaReunionAnterior.actionItems.map((actionItem,index) =>
-                                <li key={`action-item-${index}`}>
-                                    <p> {actionItem} </p>
-                                </li>)} 
-                            </ul>
-                        </li>)}
-                    </ListaTemasARepasar>
-                </Descripcion>
-            </DescripcionTemaContainer>
+      <DescripcionTemaContainer>
+        <Titulo>{this.props.tema.titulo}</Titulo>
+        <Descripcion>
+          <ListaTemasARepasar>
+            {this.props.tema.temasParaRepasar.map((temaReunionAnterior, index) =>
+              <li key={`tema-a-repasar-${index}`}>
+                <b> {temaReunionAnterior.tema.titulo} </b>
+                {temaReunionAnterior.actionItems.length !== 0 ?
+                  <ul>
+                    {temaReunionAnterior.actionItems.map((actionItem, index) =>
+                      <li key={`action-item-${index}`}>
+                        <p> {actionItem.descripcion}.
+                          <b> Responsables </b>: {actionItem.responsables.map((responsable, index) =>
+                            (index < actionItem.responsables.length - 1) ? responsable.name + ', ' : responsable.name)
+                          } </p>
+                      </li>)}
+                  </ul> : <p> No hay action items para este tema</p>}
+              </li>)}
+          </ListaTemasARepasar>
+        </Descripcion>
+      </DescripcionTemaContainer>
     );
   }
 }
