6e79727774efdb384e6ca7879dd4d38aac9cdd21
Carolina <carolinadestuet@gmail.com>
2020-01-23 14:18:21 -0300

    fix subject reactions container color background

--- frontend/src/components/SubjectReactionsContainer.styled.js
+++ frontend/src/components/SubjectReactionsContainer.styled.js
@@ -6,7 +6,7 @@ export const SubjectReactionsContainer = styled.div`
     align-items: center;
     justify-content: center;
     border-top: 1px solid white;
-    background: rgb(0; 0; 0; 0.7);
+    background: rgb(0, 0, 0, 0.7);
     height: ${(props) => props.height}rem;
 `;
 
