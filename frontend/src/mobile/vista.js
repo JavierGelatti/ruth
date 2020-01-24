70612f89b88aa526359e3e01e2f132dfff911ce8
ArielUmansky <umanskyariel@gmail.com>
2020-01-24 18:59:43 -0300

    Refactor mobile view

--- frontend/src/mobile/vista.js
+++ frontend/src/mobile/vista.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import {
-  faThumbsUp, faThumbsDown, faHashtag, faSync
+  faThumbsUp, faThumbsDown, faHashtag, faSync, faHandPaper, faSignOutAlt,
 } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import ParticipantsCard from '../cola-de-participantes/ParticipantsCard';
 import { ReactionButton } from './ReactionButton';
-import { SelectPineToTalkPopUp } from './SelectPineToTalkPopUp';
 import {
-  MobileUsableArea, SubjectContainer, ParticipantsContainer, 
-  HeaderContainer, SubjectTitle, TitleName
+  MobileUsableArea, SubjectContainer, ParticipantsContainer,
+  HeaderContainer, SubjectTitle, TitleName, ActionContainerStyle, StyledButton, ActionText,
 } from './vista.styled';
-import SpeakingActions from './SpeakingActions';
-import { SubjectReactionsContainer } from '../components/SubjectReactionsContainer.styled';
-import { InteractionsContainer } from '../components/InteractionsContainer.styled';
+import { ReactionsContainer } from '../components/SubjectReactionsContainer.styled';
+import { CardInteractionsContainer } from '../components/InteractionsContainer.styled';
 
 const hardcodedParticipant = { inicio: Date.now(), fin: null, nombre: 'Ari Hablando' };
 
@@ -24,9 +23,6 @@ class Vista extends React.Component {
     subjectSlackClicked: false,
     subjectRecommendingEndingClicked: false,
     wannaTalk: false,
-    wantOtherToTalk: false,
-    selectedPine: null,
-    availablePines: ['fulanito', 'menganito'], // TODO: cambiar por los pinos logueados
   };
 
   onSubjectThumbsUpClick = () => {
@@ -59,84 +55,49 @@ class Vista extends React.Component {
     this.setState({ wannaTalk: false });
   };
 
-  onWantOtherPersonToTalk = () => {
-    this.setState({ wantOtherToTalk: !this.state.wantOtherToTalk });
-  };
-
-  handlePineChange = (event) => {
-    this.setState({ selectedPine: event.target.id });
-  };
-
   render() {
     return (
       <MobileUsableArea>
         <HeaderContainer>
           <TitleName> 10Pines Roots </TitleName>
         </HeaderContainer>
-        <InteractionsContainer height="30%" 
-          imgURL="url(https://images.assetsdelivery.com/compings_v2/lexanda/lexanda1704/lexanda170400030.jpg)">
+        <CardInteractionsContainer height="30%"
+                                   imgURL="url(https://images.assetsdelivery.com/compings_v2/lexanda/lexanda1704/lexanda170400030.jpg)">
           <SubjectTitle>
             {getTemaById(this.props.tema)}
           </SubjectTitle>
-          <SubjectReactionsContainer height={6}>
-            <ReactionButton isBig isActive={this.state.subjectThumbsUpClicked} 
-              isDisabled={this.state.subjectThumbsDownClicked} icon={faThumbsUp} 
+          <ReactionsContainer height={6}>
+            <ReactionButton isBig isActive={this.state.subjectThumbsUpClicked}
+              isDisabled={this.state.subjectThumbsDownClicked} icon={faThumbsUp}
               onClick={this.onSubjectThumbsUpClick} />
-            <ReactionButton isBig isActive={this.state.subjectThumbsDownClicked} 
-              isDisabled={this.state.subjectThumbsUpClicked} icon={faThumbsDown} 
+            <ReactionButton isBig isActive={this.state.subjectThumbsDownClicked}
+              isDisabled={this.state.subjectThumbsUpClicked} icon={faThumbsDown}
               onClick={this.onSubjectThumbsDownClick} />
-            <ReactionButton isBig isActive={this.state.subjectSlackClicked} icon={faHashtag} 
+            <ReactionButton isBig isActive={this.state.subjectSlackClicked} icon={faHashtag}
               onClick={this.onSubjectSlackClick} />
-            <ReactionButton isBig isActive={this.state.subjectRecommendingEndingClicked} icon={faSync} 
+            <ReactionButton isBig isActive={this.state.subjectRecommendingEndingClicked} icon={faSync}
               onClick={this.onSubjectRecommendingEndingClicked} />
-          </SubjectReactionsContainer>
-        </InteractionsContainer>
+          </ReactionsContainer>
+        </CardInteractionsContainer>
         <ParticipantsContainer>
-          <ParticipantsCard interactive={true} dispatch={this.props.dispatch}
-            participant={hardcodedParticipant} isParticipantTalking={true} />
-          <SpeakingActions onWannaStopTalk = {this.onWannaStopTalkClick}
-            onWannaTalk = {this.onWannaTalkClick} onWantOtherPersonToTalk = {this.onWantOtherPersonToTalk}
-            wannaTalk = {this.state.wannaTalk}
-          />
+          <ParticipantsCard interactive isParticipantTalking dispatch={this.props.dispatch} participant={hardcodedParticipant} />
+          <ActionContainerStyle>
+            {
+              !this.state.wannaTalk
+                ? <StyledButton onClick={this.onWannaTalkClick} variant={'contained'} >
+                    <ActionText> Quiero Hablar </ActionText>
+                    <FontAwesomeIcon icon={faHandPaper} color={'black'} size={'2x'} />
+                </StyledButton>
+                : <StyledButton color={'secondary'} onClick={this.onWannaStopTalkClick} variant={'contained'} >
+                    <ActionText> Dejar de Hablar </ActionText>
+                    <FontAwesomeIcon icon={faSignOutAlt} color={'white'} size={'2x'} />
+                </StyledButton>
+            }
+          </ActionContainerStyle>
         </ParticipantsContainer>
-        <SelectPineToTalkPopUp
-          isDisabled={!this.state.wantOtherToTalk}
-          onExit={() => this.setState({ wantOtherToTalk: false })}
-          availablePines={this.state.availablePines}
-          handlePineChange={this.handlePineChange}
-        />
       </MobileUsableArea>
     );
   }
 }
-
-/*
-Debug items
-
-            <TextField
-              style={{backgroundColor: "white", width: "10rem", marginLeft: "1rem"}}
-              id="filled-textarea"
-              label={"Nombre de Orador"}
-              value={this.props.orador}
-              onChange={this.props.onOradorChange}
-              variant="filled"
-            />
-
-
-                        <Button onClick={() => this.props.dispatch({ tipo: 'Reiniciar reacciones' })}>Reiniciar reacciones</Button>
-
-                <Button onClick={() => this.props.dispatch({ tipo: 'Quiero Hablar' })}>
-                  <FontAwesomeIcon icon={faHandHolding} size={"3x"} color={"grey"}/>
-                </Button>
-                <Button onClick={() => this.props.dispatch({ tipo: 'Quiero Desencolarme' })}>
-                  <FontAwesomeIcon icon={faSync} size={"3x"} color={"grey"}/>
-                </Button>
-                <Button onClick={() => this.props.dispatch({ tipo: 'Quiero Dejar de Hablar' })}>
-                  <FontAwesomeIcon icon={faSync} size={"3x"} color={"grey"}/>
-                </Button>
-
-
- */
-
 
 export default Vista;
