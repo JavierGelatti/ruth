2f99167492a36aad17a66b597b1af8fe6463de2f
ailen munoz <ailenmunozz@gmail.com>
2020-01-24 00:17:24 -0300

    Update login
    - Agrega variables de ambiente para la url y el secret
    - Redirige directamente al login cuando intenta iniciar
    - Agrega el login solo para prod

--- frontend/src/Authenticator.js
+++ frontend/src/Authenticator.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { createBrowserHistory } from 'history';
-import { Route, Router } from 'react-router-dom';
+import {createBrowserHistory} from 'history';
+import {Redirect, Route, Router} from 'react-router-dom';
 import App from './App';
-import Login from './login/Login';
 import Backend from './api/backend';
 import Oradores from './oradores';
 import Mobile from './mobile';
@@ -14,26 +13,28 @@ export default class Authenticator extends React.Component {
   state = {
     usuario: null,
     cargando: true,
-  }
+  };
 
   componentDidMount() {
     Backend.getPerfil()
-      .then((usuario) => this.setState({ usuario }))
+      .then((usuario) => this.setState({usuario}))
       .catch((error) => error.response.status !== 403 && Promise.reject(error))
-      .finally(() => this.setState({ cargando: false }));
+      .finally(() => this.setState({cargando: false}));
   }
 
   render() {
-    const { cargando, usuario } = this.state;
-
+    const {cargando, usuario} = this.state;
     if (cargando) return <h1>Cargando</h1>;
-    if (!usuario) return <Login />;
     return (
       <Router history={history}>
-        <Route path="/" render={({ location }) => <App location={location}/>}/>
-        <Route path="/mobile" render={({ location }) => <Mobile location={location}/>}/>
-        <Route path="/oradores" render={({ location }) => <Oradores location={location}/>}/>
-        <Route path="/chart" render={({ location }) => <TestChart location={location}/>}/>
+        { !usuario?
+          <Route component={ () => {global.window.location.href=`https://${process.env.REACT_APP_BACKOFFICE_URL}/auth/sign_in?redirect_url=${process.env.REACT_APP_BACKOFFICE_REDIRECT_URL}/api/auth/callback&app_id=ruth-app`}} /> :
+          (<>
+          <Route path="/" render={({location}) => <App location={location}/>}/>
+          <Route path="/mobile" render={({location}) => <Mobile location={location}/>}/>
+          <Route path="/oradores" render={({location}) => <Oradores location={location}/>}/>
+          <Route path="/chart" render={({location}) => <TestChart location={location}/>}/>
+          </>)}
       </Router>
     );
   }
