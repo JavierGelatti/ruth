2c2b9990108a18e609c49f97e6097b484cfd9612
ailen munoz <ailenmunozz@gmail.com>
2020-02-13 22:28:51 -0300

    Linter

--- frontend/src/mobile/vista.js
+++ frontend/src/mobile/vista.js
@@ -4,19 +4,19 @@ import { Button } from '@material-ui/core';
 class Vista extends React.Component {
   state = {
     orador: '',
-  }
+  };
 
   render() {
     return (
-            <div>
-                <h1>Mobile</h1>
-                <Button onClick={() => this.props.dispatch({ tipo: 'Quiero Hablar' })}>Quiero Hablar</Button>
-                <Button onClick={() => this.props.dispatch({ tipo: 'Quiero Desencolarme' })}>Quiero Desencolarme</Button>
-                <Button onClick={() => this.props.dispatch({ tipo: 'Quiero Dejar de Hablar' })}>Quiero Dejar de Hablar</Button>
-                <Button onClick={() => this.props.dispatch({ tipo: 'Reiniciar reacciones' })}>Reiniciar reacciones</Button>
-                <Button onClick={() => this.props.dispatch({ tipo: 'Reaccionar', reaccion: '👍' })}>👍</Button>
-                <Button onClick={() => this.props.dispatch({ tipo: 'Reaccionar', reaccion: '👎' })}>👎</Button>
-            </div>
+      <div>
+        <h1>Mobile</h1>
+        <Button onClick={() => this.props.dispatch({ tipo: 'Quiero Hablar' })}>Quiero Hablar</Button>
+        <Button onClick={() => this.props.dispatch({ tipo: 'Quiero Desencolarme' })}>Quiero Desencolarme</Button>
+        <Button onClick={() => this.props.dispatch({ tipo: 'Quiero Dejar de Hablar' })}>Quiero Dejar de Hablar</Button>
+        <Button onClick={() => this.props.dispatch({ tipo: 'Reiniciar reacciones' })}>Reiniciar reacciones</Button>
+        <Button onClick={() => this.props.dispatch({ tipo: 'Reaccionar', reaccion: '👍' })}>👍</Button>
+        <Button onClick={() => this.props.dispatch({ tipo: 'Reaccionar', reaccion: '👎' })}>👎</Button>
+      </div>
     );
   }
 }
--- frontend/src/tipos-vista-principal/TemaActual.js
+++ frontend/src/tipos-vista-principal/TemaActual.js
@@ -11,14 +11,13 @@ import { Button, SecondaryButton } from '../components/Button.styled';
 import Countdown from '../reunion/Countdown';
 
 class TemaActual extends React.Component {
-
   static canHandleView = (view) => view === 'Tema Actual'
 
   render() {
     const { tema } = this.props;
     return (
       <TemaActualContainer>
-        <InfoTema tema = {tema}/>
+        <InfoTema tema={tema} />
         <VistaDelMedioContainer>
           {(new HandlerTipoTema()).handleTipoTema(tema)}
           <Botonera>
