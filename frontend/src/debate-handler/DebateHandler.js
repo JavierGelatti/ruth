ec30b298b66f55267baad726267cac1e7ea94662
Lucas David Traverso <lucas6246@gmail.com>
2020-02-21 00:50:57 -0300

    Unwire things that shouldn't be wired with redux

--- frontend/src/debate-handler/DebateHandler.js
+++ frontend/src/debate-handler/DebateHandler.js
@@ -1,46 +1,31 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import DebateView from './DebateView';
 import { colors } from '../styles/theme';
 
-const isTalking = (participant) => participant.inicio !== null && participant.fin === null;
-
 class DebateHandler extends React.Component {
+  dataBar = () => ({
+    data: this.props.tema.reacciones,
+    color: colors.primary,
+  });
+
+  isTalking = (participant) => participant.inicio !== null && participant.fin === null;
+
+  debateData = () => ({
+    participants: this.props.tema.oradores,
+    dataBar: this.dataBar(),
+    dataLine: { data: [] },
+  });
+
   render() {
     return (
-        <DebateView
-          debateData={this.props.debateData}
-          segundosRestantes={this.props.segundosRestantes}
-          temaActivo={this.props.temaActivo}
-          tema={this.props.tema}
-          isTalking={isTalking}/>
+      <DebateView
+        debateData={this.debateData()}
+        segundosRestantes={this.props.segundosRestantes}
+        temaActivo={this.props.temaActivo}
+        tema={this.props.tema}
+        isTalking={this.isTalking}/>
     );
   }
 }
 
-const mapStateToProps = (state) => {
-  let temaActual = null;
-  temaActual = state.temas.find((tema) => tema.fin === null && tema.inicio !== null);
-  if (!temaActual) {
-    const temasTratados = state.temas.filter((tema) => tema.fin !== null && tema.inicio !== null);
-    temasTratados.sort((t1, t2) => t1.fin.localeCompare(t2.fin));
-    temaActual = temasTratados[0];
-  }
-
-  if (!temaActual) {
-    temaActual = {};
-  }
-
-  return ({
-    debateData: {
-      participants: temaActual.oradores,
-      dataBar: {
-        data: temaActual.reacciones,
-        color: colors.primary,
-      },
-      dataLine: { data: [] },
-    },
-  });
-};
-
-export default connect(mapStateToProps)(DebateHandler);
+export default DebateHandler;
